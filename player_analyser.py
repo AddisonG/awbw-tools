57fb674ad4136f31541731aa1c8a9fd99c810d0a
Addison Gourluck <addison@gourluck.com>
2021-11-29 21:44:53 +1000

    Added ability to analyse income and properties captured
    The analyse_turn function now supports the above.
    
    Removed some commented code, and added better error handling.

--- player_analyser.py
+++ player_analyser.py
@@ -49,10 +49,10 @@ def get_map_replays(map_id: str, game_type: GameType = GameType.ALL):
 
 
 def analyse_game(game_id: str, player: str, turns: int):
+    # Get the first turn, so we can find info like player name, CO, etc.
     body = {
         "gameId": game_id,
         "turn": 0,
-        "initial": True,
     }
 
     response = requests.post(
@@ -60,9 +60,12 @@ def analyse_game(game_id: str, player: str, turns: int):
         cookies=cookie,
         json=body,
     )
-    players = list(response.json()["gameState"]["players"].values())
+    try:
+        players = list(response.json()["gameState"]["players"].values())
+    except Exception:
+        return {}
 
-    if player == 0 or player == players[0]["users_username"]:
+    if player == "0" or player == players[0]["users_username"]:
         player_num = 0
     else:
         player_num = 1
@@ -73,13 +76,12 @@ def analyse_game(game_id: str, player: str, turns: int):
     all_units = {}
     unit_count = 0
     for turn in range(0 + player_num, (turns * 2) + player_num, 2):
-        units = analyse_turn(game_id, turn)
+        units, captures, income = analyse_turn(game_id, turn)
         if units is None:
             if turn <= 1:
                 print("No game info")
                 return {}
             break
-        # print(units)
         for unit, num in units.items():
             if unit == "Infantry":
                 continue
@@ -104,7 +106,6 @@ def analyse_turn(game_id: str, turn: int):
     body = {
         "gameId": game_id,
         "turn": turn,
-        "initial": True, # What does this do?
     }
 
     response = requests.post(
@@ -119,32 +120,39 @@ def analyse_turn(game_id: str, turn: int):
         print(response.json()["message"])
         return None
 
-    # players = list(response.json()["gameState"]["players"].values())
+    units_built, captures = analyse_actions(response.json())
 
-    # player_1 = "Player 1 - " + players[0]["users_username"] + " (" + players[0]["co_name"] + ")."
-    # player_2 = "Player 2 - " + players[1]["users_username"] + " (" + players[1]["co_name"] + ")."
+    # Funds leftover from LAST turn
+    pid = str(response.json()["gameState"]["currentTurnPId"])
+    player_info = response.json()["gameState"]["players"][pid]
+    funds = player_info["players_funds"]
+    income = player_info["players_income"]
+    print(f"Day {turn//2}. ${funds - income} leftover + ${income}. Captures: {captures}.")
 
-    # print("Day: " + str(response.json()["day"]) + ". " + (player_2 if turn % 2 else player_1))
+    return units_built, captures, income
 
-    # players_income
-    # players_funds
-
+def analyse_actions(response):
     units_built = {}
-    for action in response.json()["actions"]:
-        # print_action(action)
+    captures = 0
 
+    for action in response["actions"]:
         action_type = action["action"]
+        # BUILD
         if action_type == "Build":
             unit_name = action["newUnit"]["units_name"]
             if unit_name in units_built:
                 units_built[unit_name] += 1
             else:
                 units_built[unit_name] = 1
+        # CAPTURE
+        if action_type == "Capt":
+            if action["buildingInfo"]["buildings_capture"] == 20:
+                captures += 1
 
-    return units_built
+    return units_built, captures
 
 
-def print_action(action):
+def debug_action(action):
     action_type = action["action"]
 
     if action_type == "Build":
@@ -182,5 +190,6 @@ if __name__ == "__main__":
     try:
         for replay in replays:
             analyse_game(replay, player_name, 12)
-    except Exception:
+    except Exception as e:
         print("Exception. Exiting.")
+        print(e)
